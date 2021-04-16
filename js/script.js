9f9b4c8211b74592255152382abf09fcfc985d14
Sabina <sabina.abbasova2909@gmail.com>
2021-04-16 05:58:24 +0400

    header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,78 @@
 // const languagesContainer = document.querySelector('li.languages')
+
+
+// NAVIGATION
+// Toggle menu
+const hamburgerMenuButton = document.querySelector('header.small-screen .menu-btn');
+const menuContainer = document.querySelector('header.small-screen nav.menu-nav');
+const menuContactLists = document.querySelectorAll('header.small-screen nav ul.contact li');
+
+const menuIcon = document.querySelector('header.small-screen .menu-btn .menu-icon');
+
+
+let isMenuOpened = false;
+
+hamburgerMenuButton.addEventListener("click", checkMenuIsOpened);
+function checkMenuIsOpened(){
+    if(isMenuOpened === false){
+        openMenu()
+    } else if(isMenuOpened === true){
+        closeMenu()
+        // console.log("clicked")
+    }
+}
+function openMenu(){
+    isMenuOpened = true;
+    console.log("openMenu()", isMenuOpened)
+
+    // menuContainer.style.display = "flex"
+    // menuContainer.style.transform = "translate(-50%, 0px)"
+    menuIcon.classList.add('opened')
+    setTimeout(() => {
+      // menuContainer.style.clipPath = "circle(200% at 50% 0)"
+      menuContainer.style.clipPath = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)"
+
+            
+
+    }, 20)
+
+
+
+   
+}
+function closeMenu(){
+    isMenuOpened = false;
+    console.log("closeMenu()", isMenuOpened)
+
+    // menuList.classList.remove("menu-items-appear");
+    menuIcon.classList.remove('clicked')
+
+    // menuItemLink1.parentNode.classList.remove('opened')
+    
+    setTimeout(() => {
+      
+     
+
+              setTimeout(() => {
+
+                menuIcon.classList.remove('opened')
+                setTimeout(() => {
+                menuContainer.style.clipPath = "polygon(0 0, 100% 0, 100% 0, 0 0)"         
+           
+                },400)
+              },40)
+      
+    },120)
+
+  
+}
+
+
+menuItemLinks.forEach(menuItemLink => {
+  menuItemLink.addEventListener("click",()=>{
+    setTimeout(()=>{
+      closeMenu()
+    }, 400)
+  })
+   
+});
